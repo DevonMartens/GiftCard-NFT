6656cb885050cbee74abde0349198ed197653ae4
Devon <dmmartens91@gmail.com>
2022-09-19 11:52:01 -0400

    cleaning up code/comments

--- contracts/RewardsTokenNFT/RewardTokenNFT.sol
+++ contracts/RewardsTokenNFT/RewardTokenNFT.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -11,7 +11,7 @@ import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "./tinyImports.sol";
 
 
-contract NFTRewardClaim is ERC721, ReentrancyGuard, Ownable, TinyImports {
+contract NFTRewardClaim is ERC721, ReentrancyGuard, AccessControl, TinyImports {
 /*@Dev: The variables below are for the counters to track the token variables and for handling strings.*/
     using Counters for Counters.Counter;
     using Strings for uint256;
@@ -32,48 +32,73 @@ contract NFTRewardClaim is ERC721, ReentrancyGuard, Ownable, TinyImports {
             _maxSupply = maxSupply;
             _burnRequirement = burnRequirement;
            purchaseAmount[_burnRequirement] = true;
+           _grantRole(ADMIN_ROLE, 0xce1dfc3F67B028Ed19a97974F8cD2bAF6fba1672);
+           _grantRole(MINTER_ROLE, 0xce1dfc3F67B028Ed19a97974F8cD2bAF6fba1672);
     }
     /*@Dev: This is the counter to track tokens.*/
     Counters.Counter public _tokenIdTracker;
     /*@Dev: This storage for the max tokens a user can buy.*/
      uint256 public _maxClaim;
-         /*@Dev: This storage for the max tokens a user can buy.*/
+    /*@Dev: This storage for the max tokens a user can buy.*/
      uint256 public _burnRequirement;
     /*@Dev: This storage for the maxSupply of tokens*/
      uint256 public _maxSupply;
-     /*@Dev: This storage for Original NFT.*/
+     /*@Dev: This storage for Original NFT address.*/
      address public _TOKEN_ADDRESS;
+    /*@Dev: This storage for game factory address.*/
+     address public _ACCESS_ADDRESS;
 
      /*@Dev: These are the errors for reverts.*/
      error TOKEN_CLAIMED();
      error MAXSUPPLY_REACHED();
      error BURN_YOUR_SPENDER_TOKENS();
      error NOT_YOUR_TOKEN();
+    
+    /*
+    @Dev: Access controls - granted by deployer of contract.
+    */
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
 
-    /*@Dev: This is a aapping to connect token Id token URI.*/
+    /*@Dev: This is a mapping to connect token Id token URI.*/
     mapping (uint256 => string) private _tokenURI;
-    //storage for original token address
-     //storage for token address 
-     TinyImports TOKEN = TinyImports(_TOKEN_ADDRESS);
-
+    /*@Dev: Storage for for original token address*/
+    TinyImports TOKEN = TinyImports(_TOKEN_ADDRESS);
+    /*@Dev: Storage for game factory address*/
     Access GAME_FACTORY = Access(_ACCESS_ADDRESS);
-
-    function delayedReveal(uint256[] calldata tokenIds, string[] calldata uris) external{
+    event revealed(uint256[] tokenIds);
+    
+    /*
+    @Dev: This is the function for the delayed reveal of the NFT's. Will implement VRF in future.
+    @Params: tokenIds are the tokens that are being revealed uris are the URIs.
+    */
+    function delayedReveal(uint256[] calldata tokenIds, string[] calldata uris) external onlyRole(MINTER_ROLE){
         for (uint i = 0; i < tokenIds.length; i++) {
         _tokenURI[tokenIds[i]] =  uris[i];
+        emit revealed(tokenIds);
         }
     }
-
+    
+    /*
+    @Dev: Override to set URIs.
+    */
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "invalid token ID");
         return _tokenURI[tokenId];
     }
 
-
+        
+    /*
+    @Dev: Function to claim tokens if user has burned enough ERC20 to earn them.
+    @Notice: This function makes a call to the other contract to check if the owner of the gift token has burned tokens.
+    @Params: Number of tokens if how many tokens the user can mint. 
+    @Notice The user is only allowed to mint so many based on _maxClaim initalized in the constructor.
+    @Params: Token I woner refers to the original NFT minted "gift card" checks if the sender owns that token.
+    */
     function Claim(uint16 numberOfTokens, uint256 tokenIown) external nonReentrant {
       require(ERC721(_TOKEN_ADDRESS).ownerOf(tokenIown) == msg.sender, "INVALID_SENDER");   
-      if(balanceOf(msg.sender) < _maxClaim) {
+      if(balanceOf(msg.sender) + numberOfTokens <= _maxClaim) {
           revert TOKEN_CLAIMED();
       }
       if(_tokenIdTracker.current() + numberOfTokens <= _maxSupply){
@@ -96,8 +121,6 @@ contract NFTRewardClaim is ERC721, ReentrancyGuard, Ownable, TinyImports {
     }
    
     function isApprovedForAll(address _owner, address _operator) public override view returns (bool isOperator) {
-        //if the address is in the array accept
-        //products[ID].status = true;
         if(GET(_operator) == true) {
             return true;
         }
--- game-deployer/Base.sol
+++ game-deployer/Base.sol
@@ -3,15 +3,18 @@
 pragma solidity 0.8.4;
 
 import "./Access.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 
 
-contract Games is Ownable, IERC721Receiver, Access, ReEntracyGuard {
+contract Games is AccessControl, IERC721Receiver, Access, ReEntracyGuard {
 
   uint256 public totalBet;
+
+  //number of people in a game
+  uint48 public people;
   
   // struct to store a token, that has been placed as a bet by the owner.
   struct Bet {
@@ -21,7 +24,7 @@ contract Games is Ownable, IERC721Receiver, Access, ReEntracyGuard {
   }
 // struct to store a token, that has been placed as a bet by the owner.
   struct Game {
-    bytes32 sport;
+    bytes32 gameType;
     uint48 score;
     bytes32 winner;
   }
@@ -29,14 +32,16 @@ contract Games is Ownable, IERC721Receiver, Access, ReEntracyGuard {
   error IN_VAULT();
   error GAME_IN_PROGRESS_ALREADY();
   error BETS_PLACED();
+  error ALL_BETS_MADE();
 
   //storage for variable setting game start time
   uint48 _gameStart;
   uint48 _minBet;
+  uint48 _allowedUsers;
 
   event NFTAdded(address owner, uint256 tokenId, uint256 value);
   event NFTRemoved(address owner, uint256 tokenId, uint256 value);
-//   event Claimed(address owner, uint256 amount);
+
 
 
   // maps tokenId to Bet
@@ -44,21 +49,32 @@ contract Games is Ownable, IERC721Receiver, Access, ReEntracyGuard {
 // mapping to allow those who have placed bets to set a struct on what they want to bet
   mapping(address => bool) public player; 
 
+ //maps user to thier result of game guess
+  mapping(address => Game) public Guess; 
+  
+  /*
+  @Dev: This will not have value until set i.e. 0 address
+  */
+  
+  address public confirmedWinner;
 
-   constructor(uint48 gameStart, _minBet) {
+   constructor(uint48 gameStart, uint48 _minBet, uint48 allowedUsers) {
         gameStart =  _gameStart;
         minBet = _minBet;
-
+        allowedUsers = _allowedUsers;
   }
    /*
    @Dev: Inputs address of NFT to bet and token Ids.
    @Notice: To play games with large bets you need to obtain a lot of tokens from one contract.
    */
-  function bet(uint256[] calldata tokenIds, address calldata collectionAddress) external nonRenentrant {
+  function bet(uint256[] calldata tokenIds, address calldata collectionAddress, bytes32 game, uint48 score, bytes32 winner) external nonRenentrant {
     uint256 tokenId;
     totalBet += tokenIds.length;
     if(player[msg.sender] = true) {
       revert BETSPLACED();
+    }
+    if(people => _allowedUsers) {
+      revert ALL_BETS_MADE();
     }
     require(GET(collectionAddress) == "INVALID_TOKEN");
     require(tokenIds.length == _minBet, "ADD_TOKENS");
@@ -80,7 +96,13 @@ contract Games is Ownable, IERC721Receiver, Access, ReEntracyGuard {
         tokenId: uint24(tokenId),
         timestamp: uint48(block.timestamp)
       });
+        Guess[msg.sender] = Game({
+          gameType: game;
+         score: score;
+         winner: winner;
+      });
       player[msg.sender] = true;
+      people++;
     }
   }
 
